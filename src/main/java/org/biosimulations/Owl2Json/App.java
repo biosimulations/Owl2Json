4ed21da8866038a267d57a3e44f55203bcd0b0d2
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-09-23 14:07:44 -0400

    initial commit

---
+++ src/main/java/org/biosimulations/Owl2Json/App.java
@@ -0,0 +1,31 @@
+package org.biosimulations.Owl2Json;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+
+import no.s11.owlapijsonld.JsonLdOntologyFormat;
+import no.s11.owlapijsonld.JsonLdStorer;
+
+import java.io.File; // Import the File class
+import java.io.IOException; // Import the IOException class to handle errors
+
+public class App {
+    public static void main(String[] args) throws OWLOntologyStorageException, OWLOntologyCreationException {
+        String owlURL = args[0];
+        String filePath = args[1];
+
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        JsonLdStorer.register(manager);
+
+        OWLOntology ontology = manager.loadOntology(IRI.create(owlURL));
+
+        File outPut = new File(filePath);
+
+        manager.saveOntology(ontology, new JsonLdOntologyFormat(), IRI.create(outPut));
+    }
+}
