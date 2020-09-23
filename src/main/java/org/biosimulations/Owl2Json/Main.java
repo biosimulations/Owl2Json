ac3ae2bcf22ccd2b2ccfdfc5073ac022b394033e
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-09-23 15:45:34 -0400

    update build

--- src/main/java/org/biosimulations/Owl2Json/Main.java
+++ src/main/java/org/biosimulations/Owl2Json/Main.java
@@ -1,8 +1,8 @@
 package org.biosimulations.Owl2Json;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
-
 import org.semanticweb.owlapi.model.IRI;
+
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
@@ -14,7 +14,7 @@ import no.s11.owlapijsonld.JsonLdStorer;
 import java.io.File; // Import the File class
 import java.io.IOException; // Import the IOException class to handle errors
 
-public class App {
+public class Main {
     public static void main(String[] args) throws OWLOntologyStorageException, OWLOntologyCreationException {
         String owlURL = args[0];
         String filePath = args[1];
